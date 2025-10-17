5f69c7429fae05e33d5be36b4d54590982235723
Rajaby_Ramesh <Mesh89>
2025-10-17 21:01:02 +0900

    fig 7 data

---
+++ callsets/hgsvc3-clustered-regt/nygc/remove_heavy_tags.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys
+import pysam
+
+if len(sys.argv) != 3:
+    sys.stderr.write(f"Usage: {sys.argv[0]} <in.vcf.gz> <out.vcf.gz>\n")
+    sys.exit(1)
+
+in_vcf = sys.argv[1]
+out_vcf = sys.argv[2]
+
+with pysam.VariantFile(in_vcf, "r") as invcf:
+    with pysam.VariantFile(out_vcf, "wz", header=invcf.header) as outvcf:
+        for rec in invcf:
+            for tag in ("PRPOS", "PREND", "SNAME"):
+                if tag in rec.info:
+                    del rec.info[tag]
+            outvcf.write(rec)
